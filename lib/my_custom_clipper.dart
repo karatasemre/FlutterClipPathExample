fb836ba0c66dd839615dae51363dae3398fd565e
09370190 <emre_karatas@kocsistem.com.tr>
2019-10-18 10:53:59 +0300

    add new line

--- lib/my_custom_clipper.dart
+++ lib/my_custom_clipper.dart
@@ -4,10 +4,14 @@ class MyCustomClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     Path path = Path();
-    path.lineTo(0, size.height);
+   /* path.lineTo(0, size.height);
     path.quadraticBezierTo(size.width/4, size.height-60, size.width/2, size.height-30);
     path.quadraticBezierTo(3/4*size.width, size.height, size.width, size.height-40);
-    path.lineTo(size.width, 0);
+    path.lineTo(size.width, 0);*/
+
+    path.lineTo(0.0, size.height);
+    path.lineTo(size.width, size.height-40);
+    path.lineTo(size.width, 0.0);
     return path;
   }
 
